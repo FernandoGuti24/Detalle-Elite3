f02841fbc05eb5c8ed154b4a4adc0d56d65ccd7c
Fernando Parrales <tuemail@gmail.com>
2026-04-02 20:32:40 -0600

    Mejora de Email

--- server.js
+++ server.js
@@ -374,7 +374,7 @@ app.post("/api/agendar", async (req, res) => {
                         <p><strong>⏰ Hora:</strong> ${hora}</p>
                         <p><strong>📍 Dirección:</strong> ${direccion}</p>
                         <p><strong>🧼 Servicio:</strong> ${servicio}</p>
-                        <p><strong> Extras:</strong> ${extras}</p>
+                        <p><strong>➕Extras:</strong> ${extras}</p>
                         <p><strong>💰 Total:</strong> ${total}</p>
                     </div>
                 `
